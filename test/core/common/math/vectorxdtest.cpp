a0e45490671395c3733e69fc0caa9953ae968510
Leo Basov <basov_leo@web.de>
2018-06-18 20:53:28 +0200

    Implemented VectorXdTest::arithmeticOperatorTest.

--- test/core/common/math/vectorxdtest.cpp
+++ test/core/common/math/vectorxdtest.cpp
@@ -5,11 +5,11 @@ VectorXdTest::VectorXdTest(const std::string &name):UnitTest(name){
 }
 
 void VectorXdTest::constuctorTest() const{
-    std::vector<double> vals{3.0,7.0,11.0,13.0};
-    VectorXd vec1;
-    VectorXd vec2(3,3.0);
-    VectorXd vec3(vals);
-    VectorXd vec4(vec3);
+    const std::vector<double> vals{3.0,7.0,11.0,13.0};
+    const VectorXd vec1;
+    const VectorXd vec2(3,3.0);
+    const VectorXd vec3(vals);
+    const VectorXd vec4(vec3);
 
     QVERIFY(vec1.size()==0);
 
@@ -37,8 +37,8 @@ void VectorXdTest::constuctorTest() const{
 }
 
 void VectorXdTest::assignOperatorTest() const{
-    std::vector<double> vals{3.0,7.0,11.0,13.0};
-    VectorXd vec1(vals);
+    const std::vector<double> vals{3.0,7.0,11.0,13.0};
+    const VectorXd vec1(vals);
     VectorXd vec2(3,3.0);
 
     QCOMPARE(3.0,vec1.at(0));
@@ -65,10 +65,10 @@ void VectorXdTest::assignOperatorTest() const{
 }
 
 void VectorXdTest::compareOperatorTest() const{
-    std::vector<double> vals{3.0,7.0,11.0,13.0};
-    VectorXd vec1(vals);
-    VectorXd vec2(3,3.0);
-    VectorXd vec3(vec2);
+    const std::vector<double> vals{3.0,7.0,11.0,13.0};
+    const VectorXd vec1(vals);
+    const VectorXd vec2(3,3.0);
+    const VectorXd vec3(vec2);
 
     QVERIFY(vec1>vec2);
     QVERIFY(vec1>=vec2);
@@ -81,4 +81,36 @@ void VectorXdTest::compareOperatorTest() const{
     QVERIFY(vec2==vec3);
     QVERIFY(!(vec2>vec3));
     QVERIFY(!(vec2<vec3));
+}
+
+void VectorXdTest::arithmeticOperatorTest() const{
+    const double val(3.0);
+    const std::vector<double> vals1{3.0,7.0,11.0};
+    const std::vector<double> vals2{13.0,17.0,23.0};
+    const VectorXd vec1(vals1);
+    const VectorXd vec2(vals2);
+    VectorXd vec3;
+    double val2(0.0);
+
+    vec3 = vec1 + vec2;
+
+    QCOMPARE(vec3.at(0),16.0);
+    QCOMPARE(vec3.at(1),24.0);
+    QCOMPARE(vec3.at(2),34.0);
+
+    vec3 = vec1 - vec2;
+
+    QCOMPARE(vec3.at(0),-10.0);
+    QCOMPARE(vec3.at(1),-10.0);
+    QCOMPARE(vec3.at(2),-12.0);
+
+    vec3 = vec1*val;
+
+    QCOMPARE(vec3.at(0),9.0);
+    QCOMPARE(vec3.at(1),21.0);
+    QCOMPARE(vec3.at(2),33.0);
+
+    val2 = vec1*vec2;
+
+    QCOMPARE(val2,411);
 }
--- test/core/common/math/vectorxdtest.h
+++ test/core/common/math/vectorxdtest.h
@@ -14,4 +14,5 @@ private slots:
 
     void assignOperatorTest() const;
     void compareOperatorTest() const;
+    void arithmeticOperatorTest() const;
 };
