46cbe9773d3ca83745b48538cd4667c710985b16
Leo Basov <basov_leo@web.de>
2018-06-18 20:24:51 +0200

    Implmented main.

--- test/main.cpp
+++ test/main.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
 
-int main()
-{
-    std::cout << "This is the test suite main stub" << std::endl;
+#include "core/common/math/vectorxdtest.h"
+
+int main(int argc, char **argv){
+    std::vector<std::shared_ptr<UnitTest>> tests;
+    int totalErrors(0);
+
+    tests.push_back(std::make_shared<VectorXdTest>("VectorXdTest"));
+
+    for(std::shared_ptr<UnitTest> test : tests){
+        int errors = QTest::qExec(test.get(),argc,argv);
+
+        if(errors!=0){
+            QStringList list;
+            std::string fileName(test->getName() + ".txt");
+
+            list << "";
+            list << "-o";
+            list << QString::fromStdString(fileName);
+
+            QTest::qExec(test.get(),list);
+
+            totalErrors += errors;
+        }
+    }
+
+    tests.clear();
+
+    std::cout << "=============================================================" << std::endl;
+    std::cout << totalErrors << " test(s) faild." << std::endl;
+    std::cout << "=============================================================" << std::endl;
+
     return 0;
 }
