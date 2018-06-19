6b2ee8150f32ab4668071d868564cb420c49a73c
Leo Basov <basov_leo@web.de>
2018-06-19 21:28:06 +0200

    Implemented MatrixXdTest::constructorTest.

--- test/core/common/math/matrixxdtest.cpp
+++ test/core/common/math/matrixxdtest.cpp
@@ -4,3 +4,48 @@ MatrixXdTest::MatrixXdTest(const std::string& name)
                           :UnitTest(name){
 
 }
+
+void MatrixXdTest::constructorTest() const{
+    const std::vector<std::vector<double>> rows({{1.0,3.0},{7.0,11.0}});
+    const MatrixXd mat1;
+    const MatrixXd mat2(2,2,1.0);
+    const MatrixXd mat3(rows);
+    const MatrixXd mat4(mat3);
+
+    QCOMPARE(mat1.sizeRow(),size_t(0));
+    QCOMPARE(mat1.sizeColumn(),size_t(0));
+
+    QCOMPARE(mat2.sizeRow(),size_t(2));
+    QCOMPARE(mat2.sizeColumn(),size_t(2));
+
+    QCOMPARE(mat2[0][0],1.0);
+    QCOMPARE(mat2[1][0],1.0);
+    QCOMPARE(mat2[0][1],1.0);
+    QCOMPARE(mat2[1][1],1.0);
+
+    QCOMPARE(mat3.sizeRow(),size_t(2));
+    QCOMPARE(mat3.sizeColumn(),size_t(2));
+
+    QCOMPARE(mat3[0][0],1.0);
+    QCOMPARE(mat3[1][0],7.0);
+    QCOMPARE(mat3[0][1],3.0);
+    QCOMPARE(mat3[1][1],11.0);
+
+    QCOMPARE(mat3.at(0,0),1.0);
+    QCOMPARE(mat3.at(1,0),7.0);
+    QCOMPARE(mat3.at(0,1),3.0);
+    QCOMPARE(mat3.at(1,1),11.0);
+
+    QCOMPARE(mat4.sizeRow(),size_t(2));
+    QCOMPARE(mat4.sizeColumn(),size_t(2));
+
+    QCOMPARE(mat4[0][0],1.0);
+    QCOMPARE(mat4[1][0],7.0);
+    QCOMPARE(mat4[0][1],3.0);
+    QCOMPARE(mat4[1][1],11.0);
+
+    QCOMPARE(mat4.at(0,0),1.0);
+    QCOMPARE(mat4.at(1,0),7.0);
+    QCOMPARE(mat4.at(0,1),3.0);
+    QCOMPARE(mat4.at(1,1),11.0);
+}
--- test/core/common/math/matrixxdtest.h
+++ test/core/common/math/matrixxdtest.h
@@ -1,10 +1,14 @@
 #pragma once
 
 #include "../../../unittest.h"
+#include "../../../../src/core/common/math/matrixxd.h"
 
 class MatrixXdTest : public UnitTest
 {
     Q_OBJECT
 public:
     MatrixXdTest(const std::string& name);
+
+private slots:
+    void constructorTest() const;
 };
