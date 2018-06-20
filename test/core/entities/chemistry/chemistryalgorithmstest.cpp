24bbc03c15a02fbada7d8beb63667270c331c03f
leo <basov.leo@googlemail.com>
2018-06-20 11:40:55 +0200

    Added simple ChemistryAlgorithmsTest::arrheniusEquationTest.

--- test/core/entities/chemistry/chemistryalgorithmstest.cpp
+++ test/core/entities/chemistry/chemistryalgorithmstest.cpp
@@ -4,3 +4,11 @@ ChemistryAlgorithmsTest::ChemistryAlgorithmsTest(const std::string& name)
                                                 :UnitTest(name){
 
 }
+
+void ChemistryAlgorithmsTest::arrheniusEquationTest(){
+    const double preFactor(13.0);
+    const double activationEnergy(0.0);
+    const double temperature(1.0);
+
+    QCOMPARE(ChemistryAlgorithms::arrheniusEquation(preFactor,activationEnergy,temperature),preFactor);
+}
--- test/core/entities/chemistry/chemistryalgorithmstest.h
+++ test/core/entities/chemistry/chemistryalgorithmstest.h
@@ -1,10 +1,14 @@
 #pragma once
 
 #include "../../../unittest.h"
+#include "../../../../src/core/entities/chemistry/chemistryalgorithms.h"
 
 class ChemistryAlgorithmsTest : public UnitTest
 {
     Q_OBJECT
 public:
     ChemistryAlgorithmsTest(const std::string& name);
+
+private slots:
+    void arrheniusEquationTest();
 };
