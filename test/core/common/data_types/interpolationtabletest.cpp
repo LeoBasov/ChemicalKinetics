8ed373e2e0df58ae1613956c7d85e70b2c0adb7e
Leo Basov <basov.leo@googlemail.com>
2018-06-20 14:17:26 +0200

    Impemented InterpolationTable tests.

--- test/core/common/data_types/interpolationtabletest.cpp
+++ test/core/common/data_types/interpolationtabletest.cpp
@@ -4,3 +4,55 @@ InterpolationTableTest::InterpolationTableTest(const std::string& name):UnitTest
 {
 
 }
+
+void InterpolationTableTest::getSetTest() const{
+    InterpolationTable table;
+
+    double x_0(0.0);
+    double x_1(1.0);
+    double x_2(2.0);
+    double y_0(0.0);
+    double y_1(1.0);
+    double y_2(4.0);
+
+    table.addValuePair(x_1,y_1);
+    table.addValuePair(x_0,y_0);
+    table.addValuePair(x_2,y_2);
+
+    QCOMPARE(table.getValue(0.0),0.0);
+    QCOMPARE(table.getValue(1.0),1.0);
+    QCOMPARE(table.getValue(2.0),4.0);
+
+    QCOMPARE(table.getValue(0.5),0.5);
+    QCOMPARE(table.getValue(1.5),2.5);
+}
+
+void InterpolationTableTest::compareOperatorTest() const{
+    InterpolationTable table1;
+    InterpolationTable table2;
+    InterpolationTable table3;
+
+    double x_0(0.0);
+    double x_1(1.0);
+    double x_2(2.0);
+    double x_3(3.0);
+    double y_0(0.0);
+    double y_1(1.0);
+    double y_2(4.0);
+    double y_3(7.0);
+
+    table1.addValuePair(x_0,y_0);
+    table1.addValuePair(x_1,y_1);
+    table1.addValuePair(x_2,y_2);
+
+    table2.addValuePair(x_0,y_0);
+    table2.addValuePair(x_1,y_1);
+    table2.addValuePair(x_2,y_2);
+
+    table3.addValuePair(x_0,y_0);
+    table3.addValuePair(x_1,y_1);
+    table3.addValuePair(x_3,y_3);
+
+    QVERIFY(table1==table2);
+    QVERIFY(table1!=table3);
+}
--- test/core/common/data_types/interpolationtabletest.h
+++ test/core/common/data_types/interpolationtabletest.h
@@ -1,10 +1,15 @@
 #pragma once
 
 #include "../../../unittest.h"
+#include "../../../../src/core/common/data_types/interpolationtable.h"
 
 class InterpolationTableTest : public UnitTest
 {
     Q_OBJECT
 public:
     InterpolationTableTest(const std::string& name);
+
+private slots:
+    void getSetTest() const;
+    void compareOperatorTest() const;
 };
