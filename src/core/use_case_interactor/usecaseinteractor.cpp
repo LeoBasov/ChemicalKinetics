b7f5b5eb9ae8871a14306d77be12f1085070eebe
Leo Basov <basov.leo@googlemail.com>
2018-06-20 15:30:46 +0200

    Implemented UseCaseInteractor.

--- src/core/use_case_interactor/usecaseinteractor.cpp
+++ src/core/use_case_interactor/usecaseinteractor.cpp
@@ -4,3 +4,31 @@ UseCaseInteractor::UseCaseInteractor()
 {
 
 }
+
+void UseCaseInteractor::start(){
+    initialize();
+
+    while(this->abortCriterium.valid()){
+        execute();
+        report();
+    }
+}
+
+void UseCaseInteractor::initialize(){
+    //DUMMY
+}
+
+void UseCaseInteractor::execute(){
+    VectorXd concentrationDiffs;
+
+    this->state.setRateConstants(this->chemistry.getRateConstants(this->state.getTemperature()));
+    this->state.setReactionRates(this->chemistry.getReactionRates(this->state.getConcentrations(),this->state.getRateConstants()));
+
+    concentrationDiffs = this->chemistry.getConcentrationDiff(this->state.getReactionRates());
+
+    this->state.setConcentrations(this->integrator.integrate(this->state.getConcentrations(),concentrationDiffs));
+}
+
+void UseCaseInteractor::report(){
+    //DUMMY
+}
--- src/core/use_case_interactor/usecaseinteractor.h
+++ src/core/use_case_interactor/usecaseinteractor.h
@@ -1,12 +1,24 @@
 #pragma once
 
 #include "../common/data_types/state.h"
+#include "abortcriterium.h"
+#include "../entities/chemistry/chemistry.h"
+#include "../entities/integrator/integrator.h"
 
 class UseCaseInteractor
 {
 public:
     UseCaseInteractor();
 
+    void start();
+
 private:
     State state;
+    AbortCriterium abortCriterium;
+    Chemistry chemistry;
+    Integrator integrator;
+
+    void initialize();
+    void execute();
+    void report();
 };
