6245b8cb29705bf7c9735c619e0dfaed46de0598
Leo Basov <basov.leo@googlemail.com>
2018-06-20 13:23:55 +0200

    Implmented IntegratorAlgorithmsTest::timeStepTest.

--- test/core/entities/integrator/integratoralgorithmstest.cpp
+++ test/core/entities/integrator/integratoralgorithmstest.cpp
@@ -4,3 +4,12 @@ IntegratorAlgorithmsTest::IntegratorAlgorithmsTest(const std::string& name)
                                                   :UnitTest(name){
 
 }
+
+void IntegratorAlgorithmsTest::timeStepTest() const{
+    const VectorXd values(std::vector<double>{1.0,3.0,7.0});
+    const VectorXd diffs(std::vector<double>{10.0,3.0,7.0});
+    const double frac(0.1);
+    const double refVal(0.01);
+
+    QCOMPARE(IntegratorAlgorithms::timeStep(values,diffs,frac),refVal);
+}
--- test/core/entities/integrator/integratoralgorithmstest.h
+++ test/core/entities/integrator/integratoralgorithmstest.h
@@ -1,10 +1,14 @@
 #pragma once
 
 #include "../../../unittest.h"
+#include "../../../../src/core/entities/integrator/integratoralgorithms.h"
 
 class IntegratorAlgorithmsTest : public UnitTest
 {
     Q_OBJECT
 public:
     IntegratorAlgorithmsTest(const std::string& name);
+
+private slots:
+    void timeStepTest() const;
 };
