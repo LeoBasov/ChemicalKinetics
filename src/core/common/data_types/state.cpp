268b4b0332ff91c40e702dae8d6b255520dabdb5
leo <basov.leo@googlemail.com>
2018-06-20 11:11:37 +0200

    Added setter functions for class State.

--- src/core/common/data_types/state.cpp
+++ src/core/common/data_types/state.cpp
@@ -8,6 +8,19 @@ State::~State(){
 
 }
 
+void State::clear(){
+    this->speciesMap.clear();
+    this->reactionMap.clear();
+    this->concentrations.clear();
+    this->reactionRates.clear();
+    this->stoichiometricCoeffEducts.clear();
+    this->stoichiometricCoeffProducts.clear();
+    this->reactionPowers.clear();
+    this->time = 0.0;
+    this->temperature = 0.0;
+    this->cv = 0.0;
+}
+
 void State::setUp(const std::vector<std::string>& species,const std::vector<std::string>& reactions){
     clear();
     resize(reactions.size(),species.size());
@@ -20,17 +33,44 @@ void State::setUp(const std::vector<std::string>& species,const std::vector<std:
     }
 }
 
-void State::clear(){
-    this->speciesMap.clear();
-    this->reactionMap.clear();
-    this->concentrations.clear();
-    this->reactionRates.clear();
-    this->stoichiometricCoeffEducts.clear();
-    this->stoichiometricCoeffProducts.clear();
-    this->reactionPowers.clear();
-    this->time = 0.0;
-    this->temperature = 0.0;
-    this->cv = 0.0;
+void State::setConcentration(const std::string& species,const double& concentration){
+    this->concentrations.at(this->speciesMap.at(species)) = concentration;
+}
+
+void State::setConcentrations(const VectorXd& concentrations){
+    this->concentrations = concentrations;
+}
+
+void State::setReactionRate(const std::string& reaction,const double& reactionRate){
+    this->reactionRates.at(this->reactionMap.at(reaction)) = reactionRate;
+}
+
+void State::setReactionRates(const VectorXd& reactionRates){
+    this->reactionRates = reactionRates;
+}
+
+void State::setStoichiometricCoeffEduct(const std::string& species,const std::string& reaction,const double& coeff){
+    this->stoichiometricCoeffEducts.at(this->reactionMap.at(reaction),this->speciesMap.at(species)) = coeff;
+}
+
+void State::setStoichiometricCoeffProduct(const std::string& species,const std::string& reaction,const double& coeff){
+    this->stoichiometricCoeffProducts.at(this->reactionMap.at(reaction),this->speciesMap.at(species)) = coeff;
+}
+
+void State::setReactionPower(const std::string& species,const std::string& reaction,const double& pow){
+    this->reactionPowers.at(this->reactionMap.at(reaction),this->speciesMap.at(species)) = pow;
+}
+
+void State::setTime(const double& time){
+    this->time = time;
+}
+
+void State::setTemperature(const double& temperature){
+    this->temperature = temperature;
+}
+
+void State::setCv(const double& cv){
+    this->cv = cv;
 }
 
 void State::resize(const size_t& row,const size_t& column){
--- src/core/common/data_types/state.h
+++ src/core/common/data_types/state.h
@@ -10,8 +10,20 @@ public:
     State();
     ~State();
 
-    void setUp(const std::vector<std::string>& species,const std::vector<std::string>& reactions);
     void clear();
+
+    void setUp(const std::vector<std::string>& species,const std::vector<std::string>& reactions);
+
+    void setConcentration(const std::string& species,const double& concentration);
+    void setConcentrations(const VectorXd& concentrations);
+    void setReactionRate(const std::string& reaction,const double& reactionRate);
+    void setReactionRates(const VectorXd& reactionRates);
+    void setStoichiometricCoeffEduct(const std::string& species,const std::string& reaction,const double& coeff);
+    void setStoichiometricCoeffProduct(const std::string& species,const std::string& reaction,const double& coeff);
+    void setReactionPower(const std::string& species,const std::string& reaction,const double& pow);
+    void setTime(const double& time);
+    void setTemperature(const double& temperature);
+    void setCv(const double& cv);
 
 private:
     std::map<std::string,std::size_t> speciesMap;
