bad196e2223ee5e7aabaec5f85a4efcaed259260
Leo Basov <basov.leo@googlemail.com>
2018-06-21 13:23:56 +0200

    Implemented IntegratorTest::integratorTest.

--- test/core/entities/integrator/integratortest.cpp
+++ test/core/entities/integrator/integratortest.cpp
@@ -4,3 +4,45 @@ IntegratorTest::IntegratorTest(const std::string& name):UnitTest(name)
 {
 
 }
+
+void IntegratorTest::integratorTest() const{
+    const double dt = 0.1;
+    const double dtParam = 0.1;
+    const double refParamDt(0.1*(1.0/7.0));
+    const VectorXd values(std::vector<double>{1.0,3.0,7.0});
+    const VectorXd diff(std::vector<double>{7.0,3.0,1.0});
+    const VectorXd refConstDt(std::vector<double>{1.7,3.3,7.1});
+    const VectorXd refVarDt(std::vector<double>{1.0 + refParamDt*7.0,3.0 + refParamDt*3.0,7.0 + refParamDt*1.0});
+    VectorXd results;
+    Integrator integrator;
+    bool control(false);
+
+    integrator.setTimeStep(dt);
+    integrator.setTimeStepParamter(dtParam);
+
+    integrator.setMode(Integrator::const_dt);
+
+    results = integrator.integrate(values,diff);
+
+    QCOMPARE(results.at(0),refConstDt.at(0));
+    QCOMPARE(results.at(1),refConstDt.at(1));
+    QCOMPARE(results.at(2),refConstDt.at(2));
+
+    integrator.setMode(Integrator::var_dt);
+
+    results = integrator.integrate(values,diff);
+
+    QCOMPARE(results.at(0),refVarDt.at(0));
+    QCOMPARE(results.at(1),refVarDt.at(1));
+    QCOMPARE(results.at(2),refVarDt.at(2));
+
+    integrator.setMode(Integrator::none);
+
+    try{
+        results = integrator.integrate(values,diff);
+    }catch(Exception){
+        control = true;
+    }
+
+    QVERIFY(control);
+}
--- test/core/entities/integrator/integratortest.h
+++ test/core/entities/integrator/integratortest.h
@@ -1,10 +1,14 @@
 #pragma once
 
 #include "../../../unittest.h"
+#include "../../../../src/core/entities/integrator/integrator.h"
 
 class IntegratorTest : public UnitTest
 {
     Q_OBJECT
 public:
     IntegratorTest(const std::string& name);
+
+private slots:
+    void integratorTest() const;
 };
