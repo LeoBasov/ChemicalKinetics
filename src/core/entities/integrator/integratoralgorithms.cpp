a583f7f243b3b379df8e5d59f383be4562417aea
Leo Basov <basov.leo@googlemail.com>
2018-06-22 16:55:36 +0200

    Fixed bug in funtcion IntegratorAlgorithms::timeStep.

--- src/core/entities/integrator/integratoralgorithms.cpp
+++ src/core/entities/integrator/integratoralgorithms.cpp
@@ -4,8 +4,11 @@ double IntegratorAlgorithms::timeStep(const VectorXd& values,const VectorXd& dif
     double timeStep(std::numeric_limits<double>::max());
 
     for(size_t i(0);i<values.size();++i){
-        const double timeStepNew(frac*values.at(i)/diff.at(i));
-        if(timeStepNew < timeStep){
+        const double timeStepNew(std::abs(frac*values.at(i)/diff.at(i)));
+
+        if(timeStepNew==0 || diff.at(i)==0){
+            continue;
+        }else if(timeStepNew < timeStep){
             timeStep = timeStepNew;
         }
     }
