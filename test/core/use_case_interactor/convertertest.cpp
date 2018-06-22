9a8c608e9e727ac770c6cd2e3d1ea6974d263422
Leo Basov <basov.leo@googlemail.com>
2018-06-22 10:13:59 +0200

    Added ConverterTest::convertTest.

--- test/core/use_case_interactor/convertertest.cpp
+++ test/core/use_case_interactor/convertertest.cpp
@@ -4,3 +4,24 @@ ConverterTest::ConverterTest(const std::string& name)
                             :UnitTest(name){
 
 }
+
+void ConverterTest::convertTest() const{
+    const std::vector<std::string> species{"Spec1","Spec2","Spec3"};
+    const std::vector<std::string> reactions{"React1","React2"};
+
+    const std::vector<std::pair<std::string,double>> concetrations{{"Spec3",0.1},{"Spec2",0.2},{"Spec1",0.3}};
+    const std::vector<std::pair<std::string,double>> rateConstants{{"React2",0.4},{"React1",0.5}};
+    const VectorXd concetrationsVec(std::vector<double>{0.3,0.2,0.1});
+    const VectorXd rateConstantsVec(std::vector<double>{0.5,0.4});
+    VectorXd vecRes;
+
+    Converter converter;
+
+    converter.setUp(species,reactions);
+
+    vecRes = converter.vector(concetrations,Converter::species);
+
+    QCOMPARE(vecRes.at(0),concetrationsVec.at(0));
+    QCOMPARE(vecRes.at(1),concetrationsVec.at(1));
+    QCOMPARE(vecRes.at(2),concetrationsVec.at(2));
+}
--- test/core/use_case_interactor/convertertest.h
+++ test/core/use_case_interactor/convertertest.h
@@ -1,10 +1,14 @@
 #pragma once
 
 #include "../../unittest.h"
+#include "../../../src/core/use_case_interactor/converter.h"
 
 class ConverterTest : public UnitTest
 {
     Q_OBJECT
 public:
     ConverterTest(const std::string& name);
+
+private slots:
+    void convertTest() const;
 };
