3e5f4fdb08659e6b3b65f75d6e56ee793f61aff5
Leo Basov <basov.leo@googlemail.com>
2018-06-22 16:57:13 +0200

    Added functions.

--- src/applications/FirstTest/testpresenter.cpp
+++ src/applications/FirstTest/testpresenter.cpp
@@ -13,6 +13,13 @@ void TestPresenter::update(){
     const State& locSt(this->state->getState());
 
     std::cout << "Temperature = " << locSt.temperature <<  std::endl;
+
+    for(size_t i(0);i<locSt.concentrations.size();i++){
+        std::cout << locSt.speciesMap.at(i) << " = " << locSt.concentrations.at(i) << ", ";
+    }
+
+    std::cout << std::endl;
+    std::cout << "--------------------------------------------------------------------------------" << std::endl;
 }
 
 void TestPresenter::subscribe(const std::shared_ptr<SimulationState>& state){
