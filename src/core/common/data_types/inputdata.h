db083dce8bf6eba6b0a3821c74b41a74bd471e97
Leo Basov <basov_leo@web.de>
2018-06-24 16:18:00 +0200

    Refactored input data.

--- src/core/common/data_types/inputdata.h
+++ src/core/common/data_types/inputdata.h
@@ -4,12 +4,41 @@
 #include <vector>
 
 struct InputData{
-    struct IntegratorData
-    {
+    struct RateConstPair{
+        double temperature = 0.0;
+        double rateConstant = 0.0;
+    };
+
+    struct ArrheniusCoeffPair{
+        double preFactor = 0.0;
+        double activationEnergy = 0.0;
+    };
+
+    struct SpeciesValuePair{
+        std::string species;
+        double value = 0.0;
+    };
+
+    struct Reaction{
+        std::string name;
         std::string mode = "none";
 
-        double timeStep = 0.0;
-        double parameter = 0.0;
+        double reactionRate = 0.0;
+        double rateConstant = 0.0;
+        std::vector<RateConstPair> rateConstantTables;
+        ArrheniusCoeffPair arrheniusCoefficients;
+
+        std::vector<SpeciesValuePair> stoichiometricCoeffEducts;
+        std::vector<SpeciesValuePair> stoichiometricCoeffProducts;
+        std::vector<SpeciesValuePair> reactionPowers;
+    };
+
+    struct Species{
+        std::string name;
+
+        double concentration = 0.0;
+        double concentrationDiff = 0.0;
+        double temperature = 0.0;
     };
 
     struct ChemistryData{
@@ -33,6 +62,14 @@ struct InputData{
         double temperature = 0.0;
     };
 
-    IntegratorData integratorData;
+    struct IntegratorData
+    {
+        std::string mode = "none";
+
+        double timeStep = 0.0;
+        double parameter = 0.0;
+    };
+
     ChemistryData chemistryData;
+    IntegratorData integratorData;
 };
