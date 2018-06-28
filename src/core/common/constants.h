0a33ff353fdafd7b5475feeba57a625a1063b9fd
Leo Basov <basov.leo@googlemail.com>
2018-06-28 18:05:22 +0200

    Fixed bug in Constants.

--- src/core/common/constants.h
+++ src/core/common/constants.h
@@ -25,6 +25,6 @@
 namespace Constants{
 
 const double universalGasConst(8.3144598); //[J K^-1 mol^-1]
-const double boltzmannConst(1.38064852e10-23); //[J K^-1]
+const double boltzmannConst(1.38064852e-23); //[J K^-1]
 
 }
